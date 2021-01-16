7171690e846381072739ab5f33edbe25860da821
Francisco Junior <jr-francis2011@hotmail.com>
2021-01-16 00:38:38 -0300

    remove redux from app

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,29 +1,23 @@
-// eslint-disable-next-line
-import React, { useState } from "react";
-
-// const [searchTerm, setSearchTerm] = useState("");
-
-// const handleChange = (event) => {
-//   setSearchTerm(event.target.value);
-// };
+import React from "react";
+import { handleSubmit } from "./utils";
 
 const Navbar = ({ searchChange }) => {
-  return (
-    <div>
-      <nav className="navbar navbar-dark bg-dark fixed-top">
-        <h1 className="navbar-brand">Pokedex</h1>
-        <form className="form-inline" onSubmit="">
-          <input
-            className="form-control mr-sm-2"
-            type="search"
-            placeholder="Search Pokemon"
-            aria-label="search"
-            onChange={searchChange}
-          />
-        </form>
-      </nav>
-    </div>
-  );
+    return (
+        <div>
+            <nav className="navbar navbar-dark bg-dark fixed-top">
+                <h1 className="navbar-brand">Pokedex</h1>
+                <form className="form-inline" onSubmit={handleSubmit}>
+                    <input
+                        className="form-control mr-sm-2"
+                        type="search"
+                        placeholder="Search Pokemon"
+                        aria-label="search"
+                        onChange={searchChange}
+                    />
+                </form>
+            </nav>
+        </div>
+    );
 };
 
 export default Navbar;
