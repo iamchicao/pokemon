f6ca74a1884c461dac730dd1ad22ab70f7ca6322
Francisco Junior <jr-francis2011@hotmail.com>
2021-01-15 22:49:09 -0300

    Update favicon and App test

--- src/Components/Containers/App.test.js
+++ src/Components/Containers/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { searchPokemon } from "../../components/reducers/reducers";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  const store = createStore(searchPokemon);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
 });
