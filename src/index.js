8479382fa222b000a4c3f72e480a4c841ede63e4
Francisco Junior <jr-francis2011@hotmail.com>
2021-01-15 22:11:14 -0300

    Remove axios and manifest.json

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import App from "./Components/Containers/App";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
 import { searchPokemon } from "./Components/Reducers/reducers";
 
 const store = createStore(searchPokemon);
