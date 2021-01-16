fb7a6d4f60f343756d3420c949de46d5dd803b20
Francisco Junior <jr-francis2011@hotmail.com>
2021-01-16 00:07:07 -0300

    Add lazzy loading for images and remove .app class from div

--- src/Components/Containers/App.js
+++ src/Components/Containers/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import PokemonList from "../PokemonList";
 import { GlobalStyle } from "../../styles/styles";
+
 const App = () => {
   return (
-    <div className="App">
+    <>
       <GlobalStyle />
       <div className="container">
         <PokemonList />
       </div>
-    </div>
+    </>
   );
 };
 
