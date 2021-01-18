807dd75063776f60ea2e323ac5590a611b7fc0a9
Francisco Junior <jr-francis2011@hotmail.com>
2021-01-18 18:39:06 -0300

    improve padding, margin on individual card

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import Link from "next/link";
+import Head from "next/head";
 import pokedex from "../../src/data/pokedex";
 import descriptions from "../../src/data/descriptions";
 import abilities from "../../src/data/abilities";
@@ -13,6 +14,14 @@ export default function Pokemons({ pokemonData }) {
 
     return (
         <>
+            <Head>
+                <title>{pokemonData.name.english}</title>
+                <meta charSet="utf-8" />
+                <meta
+                    name="viewport"
+                    content="initial-scale=1.0, width=device-width"
+                />
+            </Head>
             <Pokemon
                 id={pokemonData.id}
                 name={pokemonData.name.english}
--- src/Components/Pokemon/styles.js
+++ src/Components/Pokemon/styles.js
@@ -1,22 +1,11 @@
 import styled from "styled-components";
 import { device } from "../../styles/media";
 
-
 export const Container = styled.div`
     margin: 0 auto;
     padding: 4rem;
     font-size: 0.75rem;
     @media ${device.tablet} {
         font-size: 1.1rem;
-    }
-`;
-
-export const PokemonDescriptionCard = styled.div`
-    margin: 0 auto;
-    padding: 0;
-    flex: 1 1 50%;
-    border: 1px solid #efefef;
-    p {
-        margin: 0 0 30px;
     }
 `;
